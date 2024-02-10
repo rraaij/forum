f960e21c489504e8301c3d705db39ba554d9d5f4
Ramon van Raaij <ramon.van.raaij@gmail.com>
2024-02-10 18:45:17 +0100

    doing the lessons

---
+++ src/app/(tabs)/menu/_layout.tsx
@@ -0,0 +1,31 @@
+import { Link, Stack } from "expo-router";
+import Colors from "@/constants/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Pressable } from "react-native";
+
+const MenuStack = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+      }}
+    >
+      <Stack.Screen name={"index"} options={{ title: "Menu" }} />
+    </Stack>
+  );
+};
+
+export default MenuStack;
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,47 @@
+import { Image, Pressable, StyleSheet, Text } from "react-native";
+import { Product } from "@/types";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+const ProductListItem = ({ product }: { product: Product }) => {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode={"contain"}
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    flex: 1,
+    margin: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+  image: { width: "100%", aspectRatio: 1 },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
